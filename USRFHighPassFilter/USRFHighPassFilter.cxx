a5ee9846c790c5c71bb5b2ffc750976bfdadc560
Sam Horvath <sam.horvath@kitware.com>
2017-04-25 11:15:02 -0400

    ENH: Added high pass filter functionality

--- USRFHighPassFilter/USRFHighPassFilter.cxx
+++ USRFHighPassFilter/USRFHighPassFilter.cxx
@@ -6,111 +6,99 @@
 
 #include "USRFHighPassFilterCLP.h"
 
-// Use an anonymous namespace to keep class types and function names
-// from colliding when module is used as shared object module.  Every
-// thing should be in an anonymous namespace except for the module
-// entry point, e.g. main()
-//
-namespace
-{
+#include "vtkImageButterworthHighPass.h"
+#include "vtkImageFFT.h"
+#include "vtkImageRFFT.h"
+#include "vtkImageCast.h"
+#include "itkImageToVTKImageFilter.h"
+#include "itkVTKImageToImageFilter.h"
+#include "vtkSmartPointer.h"
+#include "vtkImageExtractComponents.h"
 
-template <typename TPixel>
-int DoIt( int argc, char * argv[], TPixel )
-{
-  PARSE_ARGS;
 
-  typedef TPixel InputPixelType;
-  typedef TPixel OutputPixelType;
+//Large portions adapted from: http://www.vtk.org/Wiki/VTK/Examples/Cxx/Images/ImageIdealHighPass
 
-  const unsigned int Dimension = 3;
 
-  typedef itk::Image<InputPixelType,  Dimension> InputImageType;
-  typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
-
-  typedef itk::ImageFileReader<InputImageType>  ReaderType;
-
-  typename ReaderType::Pointer reader = ReaderType::New();
-
-  reader->SetFileName( inputVolume.c_str() );
-
-  typedef itk::SmoothingRecursiveGaussianImageFilter<
-    InputImageType, OutputImageType>  FilterType;
-  typename FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
-  filter->SetSigma( sigma );
-
-  typedef itk::ImageFileWriter<OutputImageType> WriterType;
-  typename WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( outputVolume.c_str() );
-  writer->SetInput( filter->GetOutput() );
-  writer->SetUseCompression(1);
-  writer->Update();
-
-  return EXIT_SUCCESS;
-}
-
-} // end of anonymous namespace
 
 int main( int argc, char * argv[] )
 {
   PARSE_ARGS;
 
-  itk::ImageIOBase::IOPixelType     pixelType;
-  itk::ImageIOBase::IOComponentType componentType;
+  const unsigned int Dimension = 2;
 
-  try
-    {
-    itk::GetImageType(inputVolume, pixelType, componentType);
+  typedef short InputPixelType;
+  typedef itk::Image<InputPixelType, Dimension> InputImageType;
 
-    // This filter handles all types on input, but only produces
-    // signed types
-    switch( componentType )
-      {
-      case itk::ImageIOBase::UCHAR:
-        return DoIt( argc, argv, static_cast<unsigned char>(0) );
-        break;
-      case itk::ImageIOBase::CHAR:
-        return DoIt( argc, argv, static_cast<signed char>(0) );
-        break;
-      case itk::ImageIOBase::USHORT:
-        return DoIt( argc, argv, static_cast<unsigned short>(0) );
-        break;
-      case itk::ImageIOBase::SHORT:
-        return DoIt( argc, argv, static_cast<short>(0) );
-        break;
-      case itk::ImageIOBase::UINT:
-        return DoIt( argc, argv, static_cast<unsigned int>(0) );
-        break;
-      case itk::ImageIOBase::INT:
-        return DoIt( argc, argv, static_cast<int>(0) );
-        break;
-      case itk::ImageIOBase::ULONG:
-        return DoIt( argc, argv, static_cast<unsigned long>(0) );
-        break;
-      case itk::ImageIOBase::LONG:
-        return DoIt( argc, argv, static_cast<long>(0) );
-        break;
-      case itk::ImageIOBase::FLOAT:
-        return DoIt( argc, argv, static_cast<float>(0) );
-        break;
-      case itk::ImageIOBase::DOUBLE:
-        return DoIt( argc, argv, static_cast<double>(0) );
-        break;
-      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
-      default:
-        std::cerr << "Unknown input image pixel component type: ";
-        std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
-        std::cerr << std::endl;
-        return EXIT_FAILURE;
-        break;
-      }
-    }
+  typedef double FFTPixelType;
+  typedef itk::Image<FFTPixelType, Dimension> FFTImageType;
 
-  catch( itk::ExceptionObject & excep )
-    {
-    std::cerr << argv[0] << ": exception caught !" << std::endl;
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-    }
+  typedef short OutputPixelType;
+  typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
+
+  typedef itk::ImageToVTKImageFilter<InputImageType> ImageToVTKType;
+  typedef itk::VTKImageToImageFilter<OutputImageType> VTKToImageType;
+
+  typedef itk::ImageFileReader<InputImageType> ReaderType;
+  typedef itk::ImageFileWriter<OutputImageType> WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName(inputVolume.c_str());
+  reader->Update(); 
+
+  ImageToVTKType::Pointer inputConverter = ImageToVTKType::New();
+  inputConverter->SetInput(reader->GetOutput());
+  inputConverter->Update();
+
+  vtkSmartPointer<vtkImageCast> originalCastFilter =
+    vtkSmartPointer<vtkImageCast>::New();
+  originalCastFilter->SetInputData(inputConverter->GetOutput());
+  originalCastFilter->SetOutputScalarTypeToShort();
+  originalCastFilter->Update();
+    
+  // Compute the FFT of the image
+  vtkSmartPointer<vtkImageFFT> fftFilter =
+    vtkSmartPointer<vtkImageFFT>::New();
+  fftFilter->SetInputConnection(originalCastFilter->GetOutputPort());
+  fftFilter->Update();
+
+  // High pass filter the FFT
+  vtkSmartPointer<vtkImageButterworthHighPass> highPassFilter =
+    vtkSmartPointer<vtkImageButterworthHighPass>::New();
+  highPassFilter->SetInputConnection(fftFilter->GetOutputPort());
+  highPassFilter->SetXCutOff(0.1);
+  highPassFilter->SetYCutOff(0.05);
+  highPassFilter->Update();
+
+  // Compute the IFFT of the high pass filtered image
+  vtkSmartPointer<vtkImageRFFT> rfftFilter =
+    vtkSmartPointer<vtkImageRFFT>::New();
+  rfftFilter->SetInputConnection(highPassFilter->GetOutputPort());
+  rfftFilter->Update();
+
+  vtkSmartPointer<vtkImageExtractComponents> extractRealFilter =
+    vtkSmartPointer<vtkImageExtractComponents>::New();
+  extractRealFilter->SetInputConnection(rfftFilter->GetOutputPort());
+  extractRealFilter->SetComponents(0);
+  extractRealFilter->Update();
+
+  // Cast the output back to short
+  vtkSmartPointer<vtkImageCast> outputCastFilter =
+    vtkSmartPointer<vtkImageCast>::New();
+  outputCastFilter->SetInputConnection(extractRealFilter->GetOutputPort());
+  outputCastFilter->SetOutputScalarTypeToShort();
+  outputCastFilter->Update();
+
+  // convert back to itk
+
+  VTKToImageType::Pointer convertToImage = VTKToImageType::New();
+  convertToImage->SetInput(outputCastFilter->GetOutput());
+  convertToImage->Update();
+
+  //set up writer
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName(outputVolume.c_str());
+  writer->SetInput(convertToImage->GetOutput());
+  writer->Update();
+  
   return EXIT_SUCCESS;
 }
